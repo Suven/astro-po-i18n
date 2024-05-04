2164f03ea988137f45dbbe1e25423fd13d05aeab
Sven <svenluecke48@gmx.de>
2024-05-04 19:55:16 +0200

    :zap: support param passing in build:all

--- crossbuild.mjs
+++ crossbuild.mjs
@@ -4,7 +4,7 @@ import { spawn } from "child_process";
 for (const locale of locales) {
   console.log(`\n\u001B[45m Building ${locale} \u001B[0m`);
   await new Promise((res) => {
-    const s = spawn("npm", ["run", "build"], {
+    const s = spawn("npm", ["run", "build", "--", ...process.argv.slice(2)], {
       env: {
         ...process.env,
         locale,
